b18d7f394548164e5158085c4b8c1a7db30bfd28
Mangesh Kamde <mangesh.kamde1007@gmail.com>
2021-08-08 16:36:25 +0530

    Added : if else data attrubute support

---
+++ Sitecore.XA.Foundation.Variants.Extension/FieldAttributes/GetAttributeForFuncField.cs
@@ -0,0 +1,91 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.RenderingVariants.Pipelines.GetAttributeTokenValue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.XA.Foundation.Variants.Extension.FieldAttributes
+{
+    public class GetAttributeForFuncField : GetAttributeTokenValueProcessor<KeyValuePair<string, string>>
+    {
+
+        public override string GetAttributeToken(KeyValuePair<string, string> field)
+        {
+
+            if (string.IsNullOrEmpty(field.Key) || string.IsNullOrEmpty(field.Value))
+                return null;
+
+            if (!field.Value.StartsWith("Func"))
+                return field.Value;
+
+            var currentItem = Sitecore.Context.Database.GetItem(field.Key);
+            return this.EvalFunc(currentItem, field.Value);
+
+        }
+
+        private string EvalFunc(Item currentItem, string FnConvention)
+        {
+            string output = string.Empty;
+            FunctionFormat functionFormat = new FunctionFormat(FnConvention);
+            string Fname = FnConvention.Split('{')[0];
+            switch (Fname)
+            {
+                case "FuncIndex":
+                    int counter = 1;
+                    foreach (Item _it in currentItem.Parent.Children)
+                    {
+                        if (_it.ID.ToString() == currentItem.ID.ToString())
+                            break;
+
+                        counter++;
+                    }
+                    if (counter.ToString() == functionFormat.CompareValue)
+                        output = functionFormat.TrueConditionValue;
+                    else
+                        output = functionFormat.FalseConditionValue;
+                    break;
+                case "FuncBool":
+                    if (currentItem.Fields[functionFormat.FuncType].Value == functionFormat.CompareValue)
+                        output = functionFormat.TrueConditionValue;
+                    else
+                        output = functionFormat.FalseConditionValue;
+                    break;
+                case "FuncContains":
+                    if (currentItem.Fields[functionFormat.FuncType].Value.Contains(functionFormat.CompareValue))
+                        output = functionFormat.TrueConditionValue;
+                    else
+                        output = functionFormat.FalseConditionValue;
+                    break;
+                case "FuncExtactMatch":
+                    if (currentItem.Fields[functionFormat.FuncType].Value == functionFormat.CompareValue)
+                        output = functionFormat.TrueConditionValue;
+                    else
+                        output = functionFormat.FalseConditionValue;
+                    break;
+                default:
+                    break;
+            }
+            return output;
+        }
+    }
+
+    public class FunctionFormat
+    {
+        public FunctionFormat()
+        {
+
+        }
+        public FunctionFormat(string funcConvention)
+        {
+            this.FuncType = funcConvention.Split('{')[1].Split('?')[0].Split('=')[0];
+            this.CompareValue = funcConvention.Split('{')[1].Split('?')[0].Split('=')[1];
+            this.TrueConditionValue = funcConvention.Split('?')[1].Split(':')[0].Replace("'", "").Replace("\"", "").Replace("{", "").Replace("}", "");
+            this.FalseConditionValue = funcConvention.Split('?')[1].Split(':')[1].Replace("'", "").Replace("\"", "").Replace("{", "").Replace("}", ""); ;
+        }
+        public string TrueConditionValue { get; set; }
+        public string FalseConditionValue { get; set; }
+        public string FuncType { get; set; }
+        public string CompareValue { get; set; }
+    }
+}
---
+++ Sitecore.XA.Foundation.Variants.Extension/VariantFields/RenderSection.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines;
+using Sitecore.XA.Foundation.RenderingVariants.Pipelines.GetAttributeTokenValue;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+namespace Sitecore.XA.Foundation.Variants.Extension.VariantFields
+{
+    public class RenderSection  : Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderSection
+    {
+        protected override string GetAttributeTokenValue(string fieldName, Item item)
+        {
+            Field field = item.Fields[fieldName];
+            if (field == null && !fieldName.StartsWith("Func"))
+                return string.Empty;
+
+            GetAttributeTokenValueArgs attributeTokenValueArgs = null;
+
+            if (fieldName.StartsWith("Func"))
+                attributeTokenValueArgs = new GetAttributeTokenValueArgs()
+                {
+                    Item = item,
+                    Field = new KeyValuePair<string, string>(item.ID.ToString(), fieldName)
+                };
+            else
+                attributeTokenValueArgs = new GetAttributeTokenValueArgs(item, field);
+
+            CorePipeline.Run("getAttributeTokenValue", (PipelineArgs)attributeTokenValueArgs);
+            return attributeTokenValueArgs.Result;
+        }
+    }
+}
